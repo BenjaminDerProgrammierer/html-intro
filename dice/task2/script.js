06280cff38a7e2790a9a836bc2e1f0e08aa5f2ac
Benjamin Hartmann <benjamin.p.hartmann@gmail.com>
2025-09-10 16:27:15 +0200

    Add task 2

---
+++ dice/task2/script.js
@@ -0,0 +1,19 @@
+let resultElement = document.querySelector('.result');
+
+let diceImages = [
+    '../assets/dice-1.svg',
+    '../assets/dice-2.svg',
+    '../assets/dice-3.svg',
+    '../assets/dice-4.svg',
+    '../assets/dice-5.svg',
+    '../assets/dice-6.svg'
+];
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+for (let i = 0; i < 2; i++) {
+    let diceRoll = rollDice();
+    resultElement.innerHTML += `<img src="${diceImages[diceRoll - 1]}" alt="Dice showing ${diceRoll}"> `;
+}
